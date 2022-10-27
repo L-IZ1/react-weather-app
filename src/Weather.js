d34d1a108ae4737e1d696c8e61cc0523e94fdca6
L-IZ1 <111218789+L-IZ1@users.noreply.github.com>
2022-10-27 20:24:56 +0200

    added form

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,12 @@
 import React from "react";
+import axios from "axios";
 
 export default function Weather() {
   return (
-  <div>
-    <h2>Hello from weather</h2>
-    </div>);
+    <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Type a city" onChange={updateCity} />
+          <input type="submit" value="Search" />
+        </form>
+      </div>);
 }
